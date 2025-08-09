a9dc7adfa4b2d68e57f697d00247c5ea3a4dcc44
Hardik Jindal <hardikj22@iitk.ac.in>
2025-08-09 19:07:50 +0530

    added stable diffusion pipeline

---
+++ stable_diff/utils.py
@@ -0,0 +1,37 @@
+import requests
+import os
+from dotenv import load_dotenv
+load_dotenv()
+STABILITY_KEY = os.getenv("STABILITY_API_KEY")
+def send_generation_request(
+    host,
+    params,
+):
+    headers = {
+        "Accept": "image/*",
+        "Authorization": f"Bearer {STABILITY_KEY}"
+    }
+
+    # Encode parameters
+    files = {}
+    image = params.pop("image", None)
+    mask = params.pop("mask", None)
+    if image is not None and image != '':
+        files["image"] = open(image, 'rb')
+    if mask is not None and mask != '':
+        files["mask"] = open(mask, 'rb')
+    if len(files)==0:
+        files["none"] = ''
+
+    # Send request
+    print(f"Sending REST request to {host}...")
+    response = requests.post(
+        host,
+        headers=headers,
+        files=files,
+        data=params
+    )
+    if not response.ok:
+        raise Exception(f"HTTP {response.status_code}: {response.text}")
+
+    return response
